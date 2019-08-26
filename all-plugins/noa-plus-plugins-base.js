d902e63a24fc957b9aee2ae915de0505a505c3df
EliteAsian <lavasnakegaming@gmail.com>
2019-08-25 20:30:18 -0400

    Updated Plugins

--- all-plugins/noa-plus-plugins-base.js
+++ all-plugins/noa-plus-plugins-base.js
@@ -2,6 +2,7 @@ function NoaPlusPlugins(noa, babylon) {
 	this.noa = noa;
 	this.babylon = babylon;
 	this.pluginList = [];
+	this.blockCustomOptions = [];
 	console.log("NoaPlusPlugins v1.0.0");
 }
 
@@ -13,4 +14,17 @@ NoaPlusPlugins.prototype.addPlugin = function(plugin) {
 	}
 	
 	this.pluginList.push(plugin);	
+}
+
+NoaPlusPlugins.prototype.registerBlock = function(id, optionsDefault, optionsCustom) {
+	this.blockCustomOptions[id] = optionsCustom;
+	return this.noa.registry.registerBlock(id, optionsDefault);
+}
+
+NoaPlusPlugins.prototype.getBlockCustomOptions = function(id, optionName) {
+	if (optionName in this.blockCustomOptions[id]) {
+		return this.blockCustomOptions[id][optionName];
+	} else {
+		return null;
+	}
 }
